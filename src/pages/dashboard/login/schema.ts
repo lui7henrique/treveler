b75e2b0570dd3c64c38e611287ecf5ad3ecd1b71
ThiagoMaurat <thiagomaurat@hotmail.com>
2022-12-15 12:51:31 -0300

    feat: Tippy.js

--- src/pages/dashboard/login/schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-  email: z.string().email("E-mail é um campo obrigatório."),
-  password: z.string().min(6, "Senha é um campo obrigatório."),
-});
